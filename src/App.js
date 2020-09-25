e67af7f9169c47eecf885e46eb86c6ae35f4d605
Wojciech Kuśmierczyk <wojciech.kusmierczyk@outlook.com>
2020-09-25 23:12:01 +0200

    Praca nad nagłówkiem aplikacji oraz hero-section.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {Header} from './layout/header/Header';
+import Logo from './images/Footer.png';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div className="container">
+                <Header/>
+
+                <div className="row">
+                    <div className="col-lg-6 hero-text">
+                        <h1>Sprzedaż hurtowa i detaliczna</h1>
+                        <h2>Art. przemysłowych i chemii gospodarczej</h2>
+                        <p>Artykuły biurowe, kosmetyki, środki higieny, środki czystości, akcesoria i sprzęt do
+                            sprzątania</p>
+                        <button className="btn">Dowiedz się więcej</button>
+                    </div>
+                    <div className="col-lg-6 hero-image">
+                        <img src={Logo} alt={''}/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/layout/header/Header.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import "./Header.scss";
+import {ReactComponent as MapMarker} from "bootstrap-icons/icons/geo-alt-fill.svg";
+import Logo from '../../images/logo.png';
+
+export const Header = () => {
+    const items = [
+        {
+            label: 'Start',
+            link: '',
+            active: true,
+        },
+        {
+            label: 'O nas',
+            link: '',
+            active: false,
+        },
+        {
+            label: 'Oferta',
+            link: '',
+            active: false,
+        },
+        {
+            label: 'Kontakt',
+            link: '',
+            active: false,
+        },
+    ];
+
+    return (
+        <header>
+            <nav className="navbar navbar-expand">
+                <a className="navbar-brand" href="/">
+                    <img src={Logo} alt="Witold Mucha - Hurtownia"/>
+                </a>
+
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        {
+                            items.map(({label, link, active}, index) => {
+                                    const navLinkClass = 'nav-link';
+                                    const activeClass = active
+                                        ? 'active'
+                                        : null;
+
+                                    const classes = [navLinkClass, activeClass].filter(Boolean).join(' ');
+
+                                    return (
+                                        <a className={classes} key={index} href={link}>{label}</a>
+                                    )
+                                }
+                            )
+                        }
+                    </div>
+
+                    <a className="navbar-nav-address-link" href="/">
+                        <div className="navbar-nav-address">
+                            <div className="icon-rounded">
+                                <MapMarker fill="white" width="1rem" height="1rem"/>
+                            </div>
+
+                            Długa 105,<br/>
+                            08-430 Żelechów
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
